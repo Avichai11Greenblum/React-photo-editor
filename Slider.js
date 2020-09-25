a1eac17ddf068c989d9020d08914cd15df5626a9
Avichai Greenblum <66025573+Avichai11Greenblum@users.noreply.github.com>
2020-09-25 14:04:35 +0300

    Update Slider.js

--- Slider.js
+++ Slider.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-export default function Slider() {
+export default function Slider( {min, max, value, handleChange} ) {
+
     return (
         <div className="Slider-container">
-            <input type="range" className="Slider"/>
+            <input
+            type="range" 
+            className="Slider"
+            min={min}
+            max={max}
+            value={value}
+            onChange={handleChange}
+            />
         </div>
     )
 };
+
